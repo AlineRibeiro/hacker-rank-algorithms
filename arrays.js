ee08501d238558b35cdf31f5a707e255ac5563f2
Aline Ribeiro <alinear.mail@gmail.com>
2021-02-09 19:21:52 -0800

    Solve Arrays-DS algorithm

---
+++ arrays.js
@@ -0,0 +1,17 @@
+const numbers = [1, 4, 3, 2];
+
+//output [2,3,4,1]
+
+function reverseArray(array) {
+  const reversedArray = [];
+  const lastIndex = array.length - 1;
+
+  for (let x = lastIndex; x >= 0; x--) {
+    reversedArray.push(array[x]);
+  }
+
+  return reversedArray;
+}
+
+console.log(reverseArray(numbers));
+x
